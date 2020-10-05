e1de54ab8400ac4424290c5547f826f5b4d9768b
Fernando Bernal Avila <fernando.bernal1133@alumnos.udg.mx>
2020-10-04 22:48:36 -0500

    Se ha hecho el promagra y cada entrada de dato le sigue el tamaño del byte.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,30 @@
 using namespace std;
 
 int main() {
-    cout << "Hola mundo" << endl; // eliminar esta línea
+    char caracter;
+    int dato;
+    short corto;
+    float flotante;
+    double doble;
+    long largo;
+
+    cin>>caracter;
+    cout << sizeof(caracter)<<endl; 
+
+    cin>>dato;
+    cout << sizeof(dato)<<endl; 
+
+    cin>>corto;
+    cout << sizeof(corto)<<endl; 
+
+    cin>>flotante;
+    cout << sizeof(flotante)<<endl; 
+
+    cin>>doble;
+    cout << sizeof(doble)<<endl; 
+
+    cin>>largo;
+    cout << sizeof(largo)<<endl; 
 
     return 0;
 }
